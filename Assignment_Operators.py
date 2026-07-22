da3189cafd8935087af6b928d0e1325c293b80cc
Sohail Momin <zelan9691@gmail.com>
2026-07-22 17:52:53 +0530

    Membership Operators

---
+++ Assignment_Operators.py
@@ -0,0 +1,12 @@
+a = 3
+print(a)
+# output = 3
+#  = is assignment operator when u assign value to something its called assignment operators
+
+a += 3
+print(a)
+# meaning a = a + 3
+# output = 6
+
+
+# is operator shows both things are at same location or not
---
+++ Membership_Operator.py
@@ -0,0 +1,17 @@
+# Membership operator batata hai ki koi cheez dusri cheez ke ander hai kya nhi
+
+x = "Delhi"
+print("D" in x)
+# output = True
+# because D alphabet is in Delhi
+
+x = "Delhi"
+print("D" not in x)
+# output = False deta
+# because D alphabet is in Delhi this is counter of in operator
+
+y = [ 1,2,3]
+print(1 in y) # output = True
+print(5 in y) # output = False
+
+# This works on List Tuple and dictionaries
