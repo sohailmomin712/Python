7b123eb0c61b4a1ea41e51133566d94a9b261ec1
Sohail Momin <zelan9691@gmail.com>
2026-07-22 16:38:24 +0530

    special literals

---
+++ literals.py
@@ -0,0 +1,7 @@
+# literals are bascially raw data given in varaible in python
+
+# special literal 
+a = None
+print(a)
+# absence of anything means none
+# use to declare variable when u dont want to give value to a variable bs usko rakhna hai usko
