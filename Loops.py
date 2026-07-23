223a28e85b99682f54d388738be891b28a827c8e
Sohail Momin <zelan9691@gmail.com>
2026-07-23 12:52:36 +0530

    while loop

---
+++ Loops.py
@@ -0,0 +1,10 @@
+# There are 2 loops in python while and for loop
+
+# lets print table using while loop 
+number = int (input("Enter the number"))
+
+i = 1
+while i<11:
+    print(number, "*" ,i, "=",number * i)
+    i+=1
+
