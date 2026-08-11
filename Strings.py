8bc1c1d80d1d63dd38fa58b9a66e6e71ed7da4a4
Sohail Momin <zelan9691@gmail.com>
2026-08-11 13:30:17 +0530

    creating strings

---
+++ Strings.py
@@ -0,0 +1,36 @@
+# Strings are sequence of characters
+# in python it is sequence of unicode characters
+
+# how to create Strings
+
+msg = 'Hello'
+print(msg)
+
+msg = "Hello"
+print(msg)
+
+# dono allowed kyu hai
+
+# msg2 = 'It's raining outside'
+# print(msg2)
+
+# this will throw error as python will not understand 
+# from where the string is starting and where it is ending
+
+# solution
+msg3 = "It's raining outside"
+print(msg3)
+
+msg4 = 'It"s raining outside'
+print(msg3)
+
+# u can also use triple quotes
+# when to use => in case of multi strings like blogs
+
+a ='''Hello World'''
+b ="""Hello Sufiyan"""
+print(a)
+print(b)
+
+message = str("Hi")
+print(message)
