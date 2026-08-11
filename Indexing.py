45f971d634a46807205a31c60f070d4d1f70cd9c
Sohail Momin <zelan9691@gmail.com>
2026-08-11 13:43:36 +0530

    indexing in python

---
+++ Indexing.py
@@ -0,0 +1,25 @@
+# Indexing
+
+c = "Hello"
+print(c)
+# output => Hello
+
+print(c[0])
+# output => H
+
+# Types of Indexing
+# 1 Positive Indexing defination
+# assume krte ho ki pehle character ka index zero hai and last tak increase krte jaate ho
+# [iske ander hamesha positive number hota hai]
+# index position left to right jaata hai
+
+# 2 Negative Indexing
+# U go from right to left
+# [iske ander negative number hota hai]
+# when to use => jab user ek lengthy string de aur aapko last character nikal k dena hai
+
+print(c[-1])
+# output => o
+
+print(c[-2])
+# output => l
